e175acab3b214ceb91c65ca5a8bdb8dcbf04f3cd
Saksham Haritash <sakshamharitash@Sakshams-MacBook-Pro.local>
2025-08-09 21:28:12 +0530

    dd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import { env } from "process";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [env.REPLIT_DOMAINS.split(",")[0]],
 };
 
 module.exports = nextConfig;
